bbd8613f286d00e57d45eee083a58bcedcbddf29
lags2022 <lguzman.58erb@outlook.com>
2023-05-10 15:57:06 -0500

    update auth

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -58,6 +58,13 @@ function AuthForm() {
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) toast.error("Invalid credentials");
+        if (callback?.ok && !callback?.error) toast.success("Logged in!");
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
